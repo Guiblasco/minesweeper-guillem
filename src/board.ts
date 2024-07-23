58699b581cb2618d30b9648c5c7536cfc6699de7
Guillem Blasco <gowet92@gmail.com>
2024-07-23 20:24:36 +0200

    Create types and install typescript for devDependencies

---
+++ src/board.ts
@@ -0,0 +1,7 @@
+import { Cell, Board } from "./types.js";
+
+const createBoard = (rows: number, columns: number): Board => {
+  for (let colum = 0; colum <= columns; colum++) {
+    for (let row = 0; row <= rows; row++) {}
+  }
+};
